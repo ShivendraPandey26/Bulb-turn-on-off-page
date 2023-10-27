e7ccbaa83798cdcd90a70c9887d0482417f17e1a
ShivendraPandey26 <aviralpandey7974@gmail.com>
2023-10-27 13:18:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bulb
+ In this website the bulb glow when push the on button and off when push the off button.
---
+++ script.js
@@ -0,0 +1,12 @@
+function bulbon() {
+    let bulbObj = document.querySelector(".bulb-off");
+    bulbObj.setAttribute("src", "bulb-on.png");
+} 
+
+function bulboff() {
+    let bulbObj = document.querySelector(".bulb-off");
+    bulbObj.setAttribute("src", "bulb-off.png");
+} 
+
+
+
